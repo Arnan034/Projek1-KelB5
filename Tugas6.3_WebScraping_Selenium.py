e9d14fab5bdf78c0979e51dac72f2a085ffe21bc
Arnan <arnanda.prasatya.tif422@polban.ac.id>
2023-02-25 20:46:06 +0700

    final Tugas6.3

---
+++ Tugas6.3_WebScraping_Selenium.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import urllib.request
+import json
+import re
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome()
+driver.get("https://m.imdb.com/chart/top/?ref_=nv_mv_250")
+
+filmList = []
+
+i = 1
+for film in driver.find_elements(By.CLASS_NAME, "media"):
+    print(film.text)
+    for img in film.find_elements(By.TAG_NAME,"img"):
+        print(img.get_attribute("src"))
+        urllib.request.urlretrieve(img.get_attribute("src"), str(i)+".png")
+        flm = film.text.replace(".", ",")
+        flm = flm.replace(")", "")
+        flm = flm.replace("\n", ",")
+        flm = flm.replace("(", ",")
+        flm = flm.split(",", 3)
+        flm[1] = flm[1].replace(" ", "")
+        flm[3] = flm[3].replace(" ", "")
+        filmList.append(
+            {"No": flm[0]+".",
+            "Gambar": "PhotoTop100Film/"+ str(i) + ".png",
+            "Judul": flm[1],
+            "Tahun": flm[2],
+            "Rating": flm[3],
+            }
+        )
+        i = i+1
+    if i > 100: 
+        break;
+
+hasil_scraping = open("Top100Film.json", "w")
+json.dump(filmList, hasil_scraping, indent = 6)
+hasil_scraping.close()
+driver.quit()
